d198947e2543bb4b632227fd64b04f5b4eca7dc6
Nithyashree <nithyanavina12@gmail.com>
2026-07-02 22:16:29 +0530

    Time: 107 ms (42.31%), Space: 119.7 MB (30.77%) - LeetHub

---
+++ 3430-maximum-and-minimum-sums-of-at-most-size-k-subarrays/3430-maximum-and-minimum-sums-of-at-most-size-k-subarrays.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+class Solution {
+    public long minMaxSubarraySum(int[] A, int k) {
+        return getContribution(A, k, true) + getContribution(A, k, false);
+    }
+
+    private long getContribution(int[] A, int k, boolean findMax) {
+        int n = A.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && (findMax ? A[stack.peek()] <= A[i] : A[stack.peek()] >= A[i])) {
+                stack.pop();
+            }
+            left[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(i);
+        }
+        stack.clear();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && (findMax ? A[stack.peek()] < A[i] : A[stack.peek()] > A[i])) {
+                stack.pop();
+            }
+            right[i] = stack.isEmpty() ? n : stack.peek();
+            stack.push(i);
+        }
+
+        long totalSum = 0;
+        for (int i = 0; i < n; i++) {
+            long L = i - left[i];
+            long R = right[i] - i;
+
+            long maxL = Math.min(L, (long) k);
+            long minL = Math.max(1L, k - R + 1);
+            long count = 0;
+
+            if (maxL >= minL) {
+                long terms = maxL - minL + 1;
+                count += (k - minL + 1 + k - maxL + 1) * terms / 2;
+                count += (minL - 1) * R;
+            } else {
+                count += maxL * R;
+            }
+            totalSum += count * A[i];
+        }
+        return totalSum;
+    }
+}
+/*class Solution {
+    public long minMaxSubarraySum(int[] A, int k) {
+        long ans = 0;
+        int n = A.length;
+        for (int i = 0; i < n; i++) {
+            int max = A[i];
+            int min = A[i];
+            int limit = Math.min(n, i + k); 
+            for (int j = i; j < limit; j++) {
+                if (A[j] > max) max = A[j];
+                if (A[j] < min) min = A[j];
+                ans += max + min;
+            }
+        }
+        return ans;
+    }
+}*/
