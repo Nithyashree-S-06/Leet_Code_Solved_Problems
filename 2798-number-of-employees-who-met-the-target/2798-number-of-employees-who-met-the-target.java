3e1c127a68d4e5461410f7123470259bbe1264a3
Nithyashree <nithyanavina12@gmail.com>
2026-07-05 22:32:59 +0530

    Time: 0 ms (100%), Space: 44.3 MB (8.39%) - LeetHub

--- 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.java
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.java
@@ -1,6 +1,6 @@
 class Solution {
     public int numberOfEmployeesWhoMetTarget(int[] hr, int t) {
-        int n=hr.length; int cnt=0;
+        int cnt=0;
         for(int emp:hr){
             if(emp>=t) cnt++;
         }
